ea40a9f54e9376b6b0e5befe767b1f59a24a6609
Katie <katie.oh@outlook.com>
2023-06-05 14:51:33 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='ZenGoBot',
+      version='1.0',
+      description='Zen Go, forked from AwesomeRengoBot',
+      packages=['distutils', 'distutils.command'],
+     )
