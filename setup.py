8fd3d10e5dc83b594253eb65e083ed516ccf6d5a
Katie <katie.oh@outlook.com>
2023-06-05 15:01:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(name='ZenGoBot',
       version='1.0',
       description='Zen Go, forked from AwesomeRengoBot',
-      packages=['sgfengine', 'rengobot'],
+      py_modules=['sgfengine', 'rengobot'],
      )
